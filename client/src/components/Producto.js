bec5daabc75aa73a00c338a560309c975e928c0b
Francisco <franciscozapi@gmail.com>
2020-09-01 18:47:38 -0300

    Componente Producto

--- client/src/components/Producto.js
+++ client/src/components/Producto.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
-export default class Producto extends React.Component {
-    constructor(props){
-        super(props);
-    }
 
-    render(){
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+export default function Producto (props) {
+    const { data } = props;
+    return (
+    <div>
+        <div>
+            {data.categories.map(el=>{
+                return (
+                    <p>{el.name}</p>
+                )
+            })}
+        </div>
+
+        <div>
+            <h1>{data.name}</h1>
+            <h4>En stock: {data.stock}</h4>
+        </div>
+
+        <div>
+            <h3 style={{'color': 'green'}}>${data.price}</h3>
+        </div>
+
+        <img style = {{'max-width': '500px', 'max-height': '500px'}}src={data.image}/>
+
+        <p>{data.description}</p>
+        <p>Garantía: {data.warranty.days && `${data.warranty.days} días,`}{data.warranty.months && `${data.warranty.months} meses y`} {data.warranty.years && `${data.warranty.years} año/s!`}</p>
+    </div>
+    )
 }
