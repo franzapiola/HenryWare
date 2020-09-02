6f6e8177959aeabc09ab00ab9a3def448f95a5bc
cruz636 <juanbarcruz96@gmail.com>
2020-09-02 15:47:26 -0300

    es solo un comentario para probar luego

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -4,7 +4,8 @@ const { Product } = require('../db.js');
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
-			res.send(products);
+			res.status(200).send(products);
+			console.log(products);
 		})
 		.catch(next);
 });
@@ -16,5 +17,45 @@ server.get('/categorias/:categoria',function(req,res,next){
 
 
 })
+
+
+/*
+server.put("/addProduct",(req,res,next)=>{
+
+	console.log("Name: ",req.query.name);
+	console.log("Price",req.query.price);
+	console.log("Descrip",req.query.desc);
+	console.log("Rating",req.query.rating);
+	console.log("Warr",req.query.warranty);
+	console.log("Stock",req.query.stock);
+	console.log("Img",req.query.img);
+
+
+	var obj ={
+		nombre : req.query.name,
+		price : req.query.price
+	}
+	
+
+	Product.findOrCreate({
+		defaults : {
+			name : req.query.name,
+			price : req.query.price,
+			desciption : req.query.desc,
+			rating : req.query.rating,
+			warranty : req.query.warranty,
+			stock : req.query.stock,
+			image : req.query.img
+		}
+	})
+
+	console.log("Database updated!")
+
+
+
+	res.status(201).send("Database Updated");
+})
+*/
+
 
 module.exports = server;
