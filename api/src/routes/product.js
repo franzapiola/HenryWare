0d3c977eb12705474bad30f46b09b287d7524493
nicookenny <nicokenny98@gmail.com>
2020-09-02 20:43:32 -0300

    ST - 22 Crear ruta que devuelva los productos de x categoría //B

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,20 +1,53 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
+const {Categories} = require('../db.js')
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
-			res.send(products);
+			res.status(200).send(products);
 		})
 		.catch(next);
 });
 
 
-//Ruta todos los productos según categoría
+
+//Ruta todos los productos según categoría --> me trae todos los products que tienen esa categoría
+
 server.get('/categorias/:categoria',function(req,res,next){
+	const {categoria} = req.params;
 
-
+	Categories.findAll({
+		where:{
+			name:categoria,
+		},
+		include:[{model:Product, as:"products"}]
+	}).then(response => res.status(200).send(response[0].products)).catch(err => res.status(404).send(err))
 
 })
+
+
+
+server.get('/search', (req, res, next) => {
+	// para buscar productos : /products/search?product={nombredeproducto} Jx
+    const product = req.query.product 
+
+    //esta linea es para mostrar lo que buscamos en la consola del server, se puede comentar. JX
+    console.log("Searching ->",product)
+
+    Product.findAll({
+        where: {
+            name: product,
+        },
+        include: [
+            {all:true}
+        ]
+    })
+    .then( products => {
+        res.status(200).send(products);
+    })
+    .catch(next);
+})
+
 
 module.exports = server;
