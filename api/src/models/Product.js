e20c369cfe26c3332c14d39274e8d103fc405321
nicookenny <nicokenny98@gmail.com>
2020-09-02 13:45:33 -0300

    mejora en modelos, tabla intermedia prod / cat, inicio de ruta categorias

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -5,7 +5,12 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('product', {
-    name: {
+    product_id:{
+      type:DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name:{
       type: DataTypes.STRING,
       allowNull: false,
     },
