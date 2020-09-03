e3aa23d23e6e5b04a9bd311a5cab5cfa834ad52d
nicookenny <nicokenny98@gmail.com>
2020-09-03 15:27:50 -0300

    ruta no perfeccionada para modificar un producto VIA BODY

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,8 +1,9 @@
 const server = require('express').Router();
 const {Op} = require('sequelize')
-
+const bodyParser = require('body-parser')
 const { Product } = require('../db.js');
 const {Categories} = require('../db.js')
+
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -15,7 +16,6 @@ server.get('/', (req, res, next) => {
 
 
 //Ruta todos los productos según categoría --> me trae todos los products que tienen esa categoría
-
 server.get('/categorias/:categoria',function(req,res,next){
 	const {categoria} = req.params;
 
@@ -27,7 +27,6 @@ server.get('/categorias/:categoria',function(req,res,next){
 	}).then(response => res.status(200).send(response[0].products)).catch(err => res.status(404).send(err))
 
 })
-
 
 
 server.get('/search', (req, res, next) => {
@@ -53,5 +52,54 @@ server.get('/search', (req, res, next) => {
     .catch(next);
 })
 
+server.put('/:id',function(req,res){
+
+    const {id} = req.params
+
+    req.body.name && (Product.update({name:req.body.name},
+        {where:{
+            product_id:id
+        }
+    }))
+
+    req.body.price && (Product.update({price:req.body.price},
+        {where:{
+            product_id:id
+        }
+    }))
+
+    req.body.description && (Product.update({description:req.body.description},
+        {where:{
+            product_id:id
+        }
+    }))
+
+    req.body.rating && (Product.update({rating:req.body.rating},
+        {where:{
+            product_id:id
+        }
+    }))
+
+    req.body.warranty && (Product.update({warranty:req.body.warranty},
+        {where:{
+            product_id:id
+        }
+    }))
+
+    req.body.stock && (Product.update({stock:req.body.stock},
+        {where:{
+            product_id:id
+        }
+    }))
+    
+    req.body.image && (Product.update({image:req.body.image},
+        {where:{
+            product_id:id
+        }
+    }))
+
+    Product.findOne({where:{product_id:id}}).then(response => res.status(200).json(response))
+})
 
 module.exports = server;
+
