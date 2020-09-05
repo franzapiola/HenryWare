613c752c6883dcb1a7a64f6564e0a74b594a6c17
cruz636 <juanbarcruz96@gmail.com>
2020-09-05 12:31:20 -0300

    jumbotron para la homepage + productos para tener en base de datos

--- api/src/db.js
+++ api/src/db.js
@@ -44,41 +44,55 @@ const { Product, User, Categories} = sequelize.models;
 //Comentado por JX.
 //Pruebas de back
 //Descomentar desde aca:
-
 /*
+
 Product.create({  
-      name : "pendrive",
-      price : 23,
-      description : "req.query.desc",
-      rating : 2,
-      warranty : 1,
-      stock : 22,
+      name : "Pendrive 32 GB",
+      price : 200,
+      description : "Pendrive de 32 GB almacenamiento. Tecnologia de punta para transferencia de datos.",
+      rating : 4,
+      warranty : 2,
+      stock : 200,
       image : "https://i.ytimg.com/vi/v4yUOeevSR0/maxresdefault.jpg"
-    }).then((res) =>{
-      // console.log({res});
-    }).catch(error => {
-      console.log({error})
     })
 
-    Product.create({  
-  name : "cdvirgen",
-  price : 0,
-  description : "para los que jugamos al lol",
+Product.create({  
+  name : "Thinkpad T-430",
+  price : 75000,
+  description : "The most popular PC in our legendary ThinkPad product line, the ThinkPad T430 is durable, secure, portable, and packed with features to enhance productivity. From its ultra-long battery life to its advanced wireless technology, the T430 helps you take your business to the next level.",
   rating : 5,
-  warranty :999,
+  warranty : 2,
+  stock : 20,
+  image : "https://www.lenovo.com/medias/lenovo-laptop-thinkpad-t430-main.png?context=bWFzdGVyfHJvb3R8NTY4Nzh8aW1hZ2UvcG5nfGg1MC9oZDIvOTUyNDAxMjE4NzY3OC5wbmd8NWIyYTg4YWU5N2Q3MmQ0MTA4NGZjZmQ4NWY1ODg2MTk5YjMyOWRjMDJiYTE0MjBjZWM5N2EyMWFjNTkxMjBhZg"
+})
+
+Product.create({  
+  name : "MacBook-Air",
+  price : 190000,
+  description : "The incredibly thin and light MacBook Air is now more powerful than ever. It features a brilliant Retina display, new Magic Keyboard, Touch ID, processors with up to twice the performance, faster graphics, and double the storage capacity.",
+  rating : 5,
+  warranty : 1,
   stock : 5,
-  image : "https://cdn.ttgtmedia.com/rms/onlineImages/storage-compact_disk_mobile.jpg"
-}).then((res) =>{
-  // console.log({res});
-}).catch(error => {
-  console.log({error})
+  image : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Macbook_Air.jpg/1200px-Macbook_Air.jpg"
 })
 
 
+Product.create({  
+  name : "Thinkpad T-480",
+  price : 150000,
+  description : "Far faster than its predecessor and has 8th generation Intel® Core™ processing.  Delivers high performance and is designed to take your productivity to new heights. Backed by the increased speed of DDR4 memory, the ThinkPad T480 gives you the power of a desktop PC and the freedom of a portable laptop.",
+  rating : 5,
+  warranty :2,
+  stock : 15,
+  image : "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/ThinkPad_T480_7.png"
+})
+
+*/
+
 //Categorias de prueba
-Categories.create({
+/*Categories.create({
   category_id : 1,
-  name : "Monitores"
+  name : "Notebooks"
 }).then((resp) =>{
       console.log({resp});
     }).catch(error => {
@@ -87,14 +101,14 @@ Categories.create({
 
 Categories.create({
   category_id : 2,
-  name : "Electrodomesticos"
+  name : "Accesorios"
 }).then((resp) =>{
       console.log({resp});
     }).catch(error => {
       console.log({error})
-    })
+    })*/
 
-*/
+
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
