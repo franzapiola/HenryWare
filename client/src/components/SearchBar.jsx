56b560838ff1b1253c58a6e189a13da4d5361924
nicookenny <nicokenny98@gmail.com>
2020-09-06 23:46:18 -0300

    estilos agregados

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {withRouter, Link} from 'react-router-dom';
 import { Navbar, Button, NavDropdown } from 'react-bootstrap'
 import Style from './navbar.css'
+import img from './LogoHW.svg'
 
 const SearchBar = (props) => {
     //Este estado almacena el contenido del input
@@ -10,7 +11,7 @@ const SearchBar = (props) => {
     return (
         <Navbar className="navbar" >
             
-            <Link to='/'><img className="brand" src="https://http2.mlstatic.com/porcellanato-eco-ipe-madera-25x100-1ra-calidad-elizabeth-D_Q_NP_735225-MLA31114916244_062019-F.webp"/></Link>
+            <Link to='/'><img className="brand" src={img}/></Link>
 
             <div className="search-bar">
 
@@ -25,7 +26,7 @@ const SearchBar = (props) => {
             <form onSubmit={(e)=>{onSearch(e, search, props)}}>
                 <input type='text' placeholder='Busca un producto...' onChange={(e)=>setSearch(e.target.value)}/>
                 
-                <Button variant='warning' type='submit'>Buscar</Button>
+                <Button className="nav-submit" type='submit'>Buscar</Button>
             </form>
             </div>
 
