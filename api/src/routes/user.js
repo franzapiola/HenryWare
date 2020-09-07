bad2ca41e71a63f45113e23470641076bc886903
Nicolas <nvelandia97@gmail.com>
2020-09-07 19:14:35 -0300

    ST-34 Ruta para crear usuario

---
+++ api/src/routes/user.js
@@ -0,0 +1,30 @@
+const server = require('express').Router()
+const bodyParser = require('body-parser')
+const { User } = require('../db.js')
+
+server.use(bodyParser.json());
+
+
+//Ruta para crear un usuario    /user    NV.
+
+
+server.post('/',(req, res) => {
+    User.create({
+        email: req.body.email,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        address: req.body.address,
+        phone_number: req.body.phone_number,
+        role: req.body.role
+    })
+    .then((usuario) => {
+        console.log(usuario);
+       res.status(201).send("Welcome!")
+        
+    })
+    .catch((error) => {
+        res.status(404).send(error)
+    })
+})
+
+module.exports = server;
