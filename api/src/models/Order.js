e9e7f83d6ec1717444b1983bb3f5f87356d314ce
Francisco <franciscozapi@gmail.com>
2020-09-08 17:53:28 -0300

    Corrección-S31/32

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -10,16 +10,11 @@ module.exports = (sequelize) => {
             primaryKey: true
         },
         state: {
-            type: DataTypes.ENUM('Created', 'Processing', 'Cancelled', 'Complete'),
-            defaultValue: 'Created',
+            type: DataTypes.ENUM('Carrito', 'Creada','Procesando', 'Cancelada', 'Completa'),
             allowNull: false
         },
         payment_method: {
             type: DataTypes.ENUM('Debito', 'Credito'),
-            allowNull: false
-        },
-        total_price: {
-            type: DataTypes.FLOAT,
             allowNull: false
         }
     })
