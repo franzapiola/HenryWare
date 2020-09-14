130fc5d6f9715970b3d71901e7c4952f6d8f1aee
nicookenny <nicokenny98@gmail.com>
2020-09-14 14:22:34 -0300

    arreglado panel de admin

--- client/src/components/admin/card.js
+++ client/src/components/admin/card.js
@@ -10,18 +10,19 @@ const Card = props => {
 
 	return(
 
-		<div className={`card text-center h-100`}>
-			<div className="overflow">
+		<div className={`card text-center h-100 w-100 d-flex`}>
+			<div className="">
 				<img src={props.imgsrc} alt="usersImg" className={`${styles.imgCard} card-img-top`}/>
 			</div>
+
 			<div className="card-body text-dark">
 				<h4 className="card-title"> {props.title}</h4>
 				<p className="card-text text-secondary">
 					{props.text}
 				</p>
-				<a href={props.linkto} className="btn btn-outline-success">ELEGIR</a>
 				
-			</div>
+				</div>
+				<a href={props.linkto} className={styles.btnAdmin} >ELEGIR</a>
 		</div>
 
 
