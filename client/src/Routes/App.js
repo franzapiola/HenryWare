12a59b53fccc0b7d205f0bf116923c103f65b52a
Alexis Enriquez <alxs.enriquez@gmail.com>
2020-09-14 12:42:21 -0300

    arreglado not found

--- client/src/Routes/App.js
+++ client/src/Routes/App.js
@@ -114,9 +114,6 @@ const App = (props) => {
         </Route>
         <Route exact path='/products/categories/addcategory' component={AddCategory}/>
         <Route path='/products/:id' component={Producto}/>
-        <Route component={NotFound} />
-      </Switch>
-        <Route path='/' component={Footer}/>
         <Route path='/cart'>
           <Cart/>
         </Route>
@@ -130,6 +127,9 @@ const App = (props) => {
         <Route exact path='/orders/table'>
           <OrdersTable/>
         </Route>
+        <Route component={NotFound} />
+      </Switch>
+        <Route path='/' component={Footer}/>
     </BrowserRouter>
     )
 }
