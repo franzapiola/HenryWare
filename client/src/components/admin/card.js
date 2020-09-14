6d5984cf055fe6394d3786009974bfe6db223db7
Juan B <juanbarcruz96@gmail.com>
2020-09-14 12:59:41 -0300

    Update card.js

--- client/src/components/admin/card.js
+++ client/src/components/admin/card.js
@@ -1,4 +1,8 @@
-mport "./card-style.css";
+import React from 'react';
+import { Navbar, Button, NavDropdown } from 'react-bootstrap'
+import { withRouter, Link } from 'react-router-dom';
+import "./card-style.css";
+import "./card-style.css";
 
 
 
