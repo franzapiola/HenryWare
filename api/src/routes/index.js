a25af7c7ab7038e69df8bbd8639ba41ea0db2ada
Gaston Ferreyra <60433890+toninence@users.noreply.github.com>
2020-09-16 01:30:02 -0300

    ST54/Crear-ruta-para-crear/agregar-reviews

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,7 +4,9 @@ const productRouter = require('./product.js');
 
 const userRouter = require('./users.js');
 
-const orderRouter = require('./orders.js')
+const orderRouter = require('./orders.js');
+
+const reviewsRouter = require('./Reviews.js');
 
 
 const router = Router();
@@ -12,10 +14,13 @@ const router = Router();
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 // router.use('/auth', authRouter);
+router.use('/reviews', reviewsRouter);
+
 router.use('/products', productRouter);
 
 router.use('/users', userRouter);
 
-router.use('/orders', orderRouter)
+router.use('/orders', orderRouter);
+
 
 module.exports = router;
