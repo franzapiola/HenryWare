ba64b3f964ad33768c61e4bf67c2e02e7fd804de
Alexis Enriquez <alxs.enriquez@gmail.com>
2020-09-16 15:54:08 -0300

    ruta para modificar productos creada

--- api/src/routes/Reviews.js
+++ api/src/routes/Reviews.js
@@ -18,8 +18,21 @@ server.get('/:product_id',function( req, res ){
     .then( response => {res.status(200).json(response)})
     .catch(err => res.status(404).json(err))
 });
+
 //Editar una Review
 server.put('/:review_id', (req, res) => {
+    const {review_id} = req.params;
+
+    const {raiting,description} = req.body
+
+    Review.update({
+        rating:raiting,
+        description:description
+    },{
+        where:{
+            review_id:review_id
+        }
+    }).then(res.status(200).send('Review modificado')).catch(err => res.status(400).send(err))
     
 })
 //Cargar un rating y descripcion a un producto
