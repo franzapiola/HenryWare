9a40ccdac486a43d1b093afadabe7412fca9b8e3
nicookenny <nicokenny98@gmail.com>
2020-09-20 17:57:53 -0300

    ???

--- client/src/components/Home/Jumbotron.jsx
+++ client/src/components/Home/Jumbotron.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import  './Jumbotron.css'
 import image from './Jumbotron.svg'
+import button from './BotonJumbotron.svg'
 import {Link} from 'react-router-dom'
 
 const Jumbotron = () =>{
 	return(
 		<div className="jumbotron jumbotron-fluid">
 			<img className='jumbotronImg' src={image} alt=""/>
-			<Link to='/products'><div className='toProducts'></div></Link>
+			<Link to='/products'><img className='jumbotronButton' src={button}/></Link>
 		</div>
 		
 		
