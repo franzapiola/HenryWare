c35e0c997154c1c8f9b849b956f9207a10075186
Nicolas <nvelandia97@gmail.com>
2020-09-20 17:21:42 -0300

    cambio-review-redux

--- client/src/redux/actions/review.jsx
+++ client/src/redux/actions/review.jsx
@@ -1,8 +1,8 @@
-export const CREATE_REVIEW = 'CREATE_REVIEW';
-export const DELETE_REVIEW = 'DELETE_REVIEW';
+import axios from "axios";
+
 export const AVERAGE_REVIEW = 'AVERAGE_REVIEW';
-export const UPDATE_REVIEW = 'UPDATE_REVIEW';
 export const ALL_REVIEW = 'ALL_REVIEW';
+
 
 
 export function allReview (payload){
@@ -17,9 +17,35 @@ export function allReview (payload){
       };
 }
 
-export const averageReview = (product_id) => {
-    return {
-        type: AVERAGE_REVIEW,
-        payload: product_id
-    }
+export function averageReview (payload){
+    return function(dispatch) {
+        return fetch(`http://localhost:3001/reviews/${payload}`)
+        .then(review => review.json())
+        .then(response => {
+
+            const sumaRating = response.map(x => {
+                return x.rating})
+            const suma = sumaRating.reduce((a, b) => {
+                return a+b}) 
+            const stars = Math.round(suma / response.length); 
+            const product_id = response[0].product_id
+
+            const obj = { stars, product_id }
+
+            return obj;
+
+        })
+        .then(obj => {
+            axios.put(`http://localhost:3001/products/${obj.product_id}`,{
+                rating: obj.stars
+            })
+            return obj.stars
+        })
+        .then(json => {
+            dispatch({ type: AVERAGE_REVIEW, 
+                     payload: json 
+                });
+          });
+      };
 }
+
