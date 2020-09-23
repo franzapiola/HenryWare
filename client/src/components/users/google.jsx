9fcad644e5dc2e8a4481321715bcb69e111467f0
Francisco <franciscozapi@gmail.com>
2020-09-23 20:05:48 -0300

    Arreglado UNO de los bugs del carrito

--- client/src/components/users/google.jsx
+++ client/src/components/users/google.jsx
@@ -7,10 +7,13 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { loadUserData } from '../../redux/actions/auth';
+import { fetchUserCart } from '../../redux/actions/cart';
 
 function Google (props) {
-    const { loadUserData } = props;
+    const { loadUserData, fetchUserCart } = props;
     const history = useHistory();
+
+    let userIdParaFetch;
 
     const responseSuccess=(googleResponse) => {
         axios.post("http://localhost:3001/auth/externalLogin?external=google",{
@@ -21,35 +24,34 @@ function Google (props) {
         .then(response =>{
             // el objeto user tiene los datos relevantes del usuario ( id, nombre, apellido,rol)
             // el objeto accessToken es el token de sesion
-            if(response.data.user){
-                //Sacamos el accessToken y la información del usuario de la respuesta
-                const { user, accessToken } = response.data
-                //Llamando a loadUserData, mandamos el usuario recibido como respuesta a la store de Redux
-                loadUserData(user);
-                //El accessToken, por otro lado, lo guardamos en el Local storage
-                localStorage.setItem("actualToken", accessToken);
-                
-                //Mandar carrito localStorage al del usuario que se está logeando con google:
-				const lStorCart = localStorage.getItem('guestCart');
-				if(lStorCart != null){
-					//Si no está vacío, osea, no es null, lo parseo
-					let currentCart = JSON.parse(lStorCart);
-					//Y a cada producto se lo mando al carrito del usuario logeado
-					currentCart.products.forEach(prod => {
-						axios.post(`http://localhost:3001/users/${user.user_id}/cart`, {
-						product_id: prod.product_id,
-						quantity: 1,
-						price: prod.price
-						});
-					});
-					//Por último, vacío el carrito local
-					localStorage.removeItem('guestCart');
-				}
-
-                //Redireccionamos a la homepage
-                return history.push('/');
-            } 
+            //Sacamos el accessToken y la información del usuario de la respuesta
+            const { user, accessToken } = response.data
+            userIdParaFetch = user.user_id;
+            //Llamando a loadUserData, mandamos el usuario recibido como respuesta a la store de Redux
+            loadUserData(user);
+            //El accessToken, por otro lado, lo guardamos en el Local storage
+            localStorage.setItem("actualToken", accessToken);
+            
+            //Mandar carrito localStorage al del usuario que se está logeando con google:
+            const lStorCart = localStorage.getItem('guestCart');
+            if(lStorCart != null){
+                //Si no está vacío, osea, no es null, lo parseo
+                let currentCart = JSON.parse(lStorCart);
+                //Y a cada producto se lo mando al carrito del usuario logeado
+                currentCart.products.forEach(prod => {
+                    axios.post(`http://localhost:3001/users/${user.user_id}/cart`, {
+                    product_id: prod.product_id,
+                    quantity: 1,
+                    price: prod.price
+                    });
+                });
+                //Por último, vacío el carrito local
+                localStorage.removeItem('guestCart');
+            }
         })
+        .then(()=>fetchUserCart(userIdParaFetch))
+        .then(()=>history.push('/'))
+        .catch(err => console.log('error en responseSuccess al logearse con Google:', err))
     }
 
     const responseFailure = (response) =>{
@@ -58,7 +60,6 @@ function Google (props) {
 
     return (
         <div>
-            <GoogleButton/>
             <GoogleLogin
             //guardarla en .env
             clientId='243862763103-9oer77jtoipa8qhd4ooflsjv5u31lmj2.apps.googleusercontent.com'
@@ -74,7 +75,8 @@ function Google (props) {
 
 const mapDispatchToProps = dispatch => {
     return {
-        loadUserData: data => dispatch(loadUserData(data))
+        loadUserData: data => dispatch(loadUserData(data)),
+        fetchUserCart: user_id => dispatch(fetchUserCart(user_id))
     }
 }
 
